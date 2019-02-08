d6509adc873e3a618052878d586903ddf9231614
Grant <grantl@beezer.com>
2019-02-08 16:26:55 +0000

    added new app file with routing

---
+++ src/components/App/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Cakes from '../Cakes';
+import Cake from '../Cake';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { cakes: [] };
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/new">New</Link>
+            </li>
+          </ul>
+          <hr />
+
+          <Route exact path="/" component={Cakes} />
+          <Route exact path="/cake/:id" component={Cake} />
+          <Route exact path="/new" />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
