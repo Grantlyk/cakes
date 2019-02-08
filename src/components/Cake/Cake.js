7aeb39dbe7adfa47785582325620203bbb157f58
Grant <grantl@beezer.com>
2019-02-08 16:26:05 +0000

    added the initial cake component

---
+++ src/components/Cake/Cake.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Cakes extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { cake: {} };
+  }
+
+  componentDidMount() {
+    this.getCake();
+  }
+
+  async getCake() {
+    const response = await fetch(
+      `http://ec2-34-243-153-154.eu-west-1.compute.amazonaws.com:5000/api/cakes/${
+        this.props.match.params.id
+      }`,
+    );
+    const cake = await response.json();
+    this.setState({ cake });
+  }
+
+  render() {
+    const cake = this.state.cake;
+    return (
+      <div key={cake.id}>
+        <span>{cake.name}</span>
+        <br />
+        <span>{cake.imageUrl}</span>
+        <br />
+        <span>{cake.yumFactor}</span>
+        <br />
+        <span>{cake.comment}</span>
+      </div>
+    );
+  }
+}
+
+export default Cakes;
